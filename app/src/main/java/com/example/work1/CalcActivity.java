d3d0208ada0e0c0428676d662d902da796db9c4c
MagerramovBoris <magerramov.boris.s@gmail.com>
2021-01-24 17:12:28 +0700

    Пункты 12

--- app/src/main/java/com/example/work1/CalcActivity.java
+++ app/src/main/java/com/example/work1/CalcActivity.java
@@ -31,5 +31,23 @@ public class CalcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calc_activity);
+
+        et_amount = findViewById(R.id.et_amount);
+        sb_percent = findViewById(R.id.sb_percent);
+
+        tv_percent = findViewById(R.id.tv_percent);
+        tv_tip = findViewById(R.id.tv_tip);
+        tv_total = findViewById(R.id.tv_total);
+        // Для текстовых полей задаём первоначальные значения
+        tv_tip.setText("0.0");
+        tv_total.setText("0.0");
+
+        // Инициализация начального значения Прогресс бара
+        double startPercent = Double.valueOf(String.valueOf(sb_percent.getProgress())) / 100.0;
+        tv_percent.setText(percentFormat.format(startPercent));
+
+        //  Слушатели событий интерфейса
+        et_amount.addTextChangedListener(amountTextWatcher);
+        sb_percent.setOnSeekBarChangeListener(sbListener);
     }
 }
